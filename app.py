072968a0967dec755fde74aa562a03470e159c0e
Yanni Vu <yvu@db8.cse.nd.edu>
2023-10-25 15:59:56 -0400

    login page

--- app.py
+++ app.py
@@ -1,20 +1,54 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-#from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask_mysqldb import MySQL
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired
 
-app = Flask(__name__) # create a new Flask instance
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
 
-app.secret_key='yvu' # secret key to setup the new instance
+app = Flask(__name__)
+app.secret_key = 'yvu'
 
-# creating a connection with mySQL
+# MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'yvu'
 app.config['MYSQL_PASSWORD'] = 'goirish'
 app.config['MYSQL_DB'] = 'yvu'
 
-@app.route('/', methods=['GET', 'POST']) # use get to get info (we are getting the user's age)
+# Initialize MySQL
+mysql = MySQL(app)
+
+@app.route('/')
 def index():
     return render_template('index.html')
-    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT username, password FROM users WHERE username = %s", (username,))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user and user[1] == password:
+            flash('Login successful', 'success')
+            return redirect(url_for('admin_page'))
+        else:
+            flash('Login failed. Please check your username and password.', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/admin_page')
+def admin_page():
+    return 'Welcome to the admin page'
+
+
 if __name__ == '__main__':
     app.debug=True
-    app.run(host='db8.cse.nd.edu', port=5070) # runs the web app (specify host and port) -opens http://db8.cse.nd.edu:5070
+    app.run(host='db8.cse.nd.edu', port=5070)
