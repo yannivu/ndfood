67cb71470621a5a67b5f0f628cb673391f88a216
Yanni Vu <yvu@nd.edu>
2023-10-25 23:56:12 -0400

    added admin tools

--- app.py
+++ app.py
@@ -44,9 +44,80 @@ def login():
             flash('Login failed. Please check your username and password.', 'danger')
     return render_template('login.html', form=form)
 
-@app.route('/admin_page')
+@app.route('/admin_page', methods=['GET'])
 def admin_page():
-    return 'Welcome to the admin page'
+    return render_template('admin_page.html')
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    cur = mysql.connection.cursor()
+    user = request.form['username']
+    pw = request.form['password']
+    # Check if the username already exists in the table
+    cur.execute("SELECT * FROM users WHERE username = %s", (user,))
+    existing_user = cur.fetchone()
+
+    if existing_user:
+        # If the user already exists, redirect to the confirmation page
+        return redirect(url_for('confirm_update', user=user, pw=pw))
+    else:
+        # If the user doesn't exist, insert a new user
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (user, pw))
+        mysql.connection.commit()
+        cur.close()
+        return f"{user} added with password: {pw}"
+
+
+@app.route('/confirm_update', methods=['GET'])
+def confirm_update():
+    user = request.args.get('user')
+    pw = request.args.get('pw')
+    return render_template('confirm_update.html', user=user, pw=pw)
+
+@app.route('/add_item', methods=['POST'])
+def add_item():
+    name = request.form['food_name']
+    cals = request.form['calories']
+    protein = request.form['protein']
+    fat = request.form['fat']
+    carbs = request.form['carbs']
+    
+    # Retrieve the selected source
+    source = request.form['source']
+
+    if source == "Dining Hall":
+        # Item is from Dining Hall
+        # Add your logic here
+        pass
+    elif source == "Grubhub":
+        # Item is from Grubhub
+        # Add your logic here
+        pass
+
+    # Rest of your code
+    return "Record updated successfully"
+
+@app.route('/search_user', methods=['POST'])
+def search_user():
+    username = request.form['username']
+
+    if not username:
+        return "Please enter a username."
+
+    # Perform a database query to retrieve information about the specified user
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+    user_info = cursor.fetchone()
+    cursor.close()
+
+    if user_info:
+        # Access the elements of the tuple by index
+        username = user_info[0]
+        password = user_info[1]
+
+        return f"User: {username} | Password: {password}"
+    else:
+        return "User not found."
 
 
 if __name__ == '__main__':
