c29fe68101de194b61e799f664fe7b2098a83f63
Yanni Vu <yvu@nd.edu>
2023-12-02 16:22:51 -0500

    updated files

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, request, session, redirect, url_for, flash, jsonify
 from flask_mysqldb import MySQL
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
+from datetime import datetime
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
@@ -89,12 +90,30 @@ def index():
 
 
     # Fetch data from the ndh_data table
-    cursor.execute("SELECT day, meal_type, name, serving_size, calories, total_fat FROM ndh_data where day='Thursday' and meal_type='Dinner'")
+    # Mapping of weekday integers to day names
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    # Get the current date
+    current_date = datetime.now()
+
+    # Get the day of the week as an integer (Monday is 0, Sunday is 6)
+    day_of_week_int = current_date.weekday()
+
+    # Get the corresponding day name using the mapping
+    day_of_week_name = days_of_week[day_of_week_int]
+
+    cursor.execute("""
+        SELECT day, meal_type, name, serving_size, calories, total_fat, total_carbohydrate, protein
+        FROM ndhfood
+        WHERE day = %s
+        AND NOT (calories = 0 AND total_fat = 0 AND total_carbohydrate = 0 AND protein = 0)
+    """, (day_of_week_name,))
+
     ndh_data = cursor.fetchall()
 
     cursor.close()
     
-    return render_template('index.html', all_data=all_data, ndh_data=ndh_data)
+    return render_template('index.html', all_data=all_data, ndh_data=ndh_data, day=day_of_week_name)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -109,7 +128,6 @@ def login():
         cursor.close()
 
         if user and user[1] == password:
-            flash('Login successful', 'success')
             return redirect(url_for('admin_page'))
         else:
             flash('Login failed. Please check your username and password.', 'danger')
@@ -223,6 +241,8 @@ def add_item():
 
 @app.route('/search_user', methods=['POST'])
 def search_user():
+    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
+
     username = request.form['username']
 
     if not username:
@@ -234,8 +254,6 @@ def search_user():
     user_info = cursor.fetchone()
     cursor.close()
 
-    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
-
     if user_info:
         # Access the elements of the tuple by index
         username = user_info[0]
@@ -244,6 +262,117 @@ def search_user():
         return f"User: {username} | Password: {password}<br>{back_button}"
     else:
         return f"User not found.<br>{back_button}"
+
+@app.route('/delete_item', methods=['POST'])
+def delete_item():
+    if request.method == 'POST':
+        item_to_delete = request.form['item_to_delete']
+        option = request.form['option']
+        if item_to_delete:
+            cursor = mysql.connection.cursor()
+            if option == "Grubhub":
+                cursor = mysql.connection.cursor()
+                cursor.execute("DELETE FROM grubhub_food WHERE name = %s", (item_to_delete,))
+                mysql.connection.commit()
+                cursor.close()
+    
+    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
+    return f"{item_to_delete} successfully deleted<br>{back_button}"
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_term = request.form.get('search_term')
+        search_pattern = f"%{search_term}%"
+
+        cursor = mysql.connection.cursor()
+        
+        cursor.execute(
+            "SELECT name, calories, protein, total_carbs, total_fat "
+            "FROM grubhub_food "
+            "WHERE name LIKE %s "
+            "UNION "
+            "SELECT name, calories, protein, total_carbohydrate as total_carbs, total_fat "
+            "FROM ndhfood "
+            "WHERE name LIKE %s",
+            (search_pattern, search_pattern)
+        )
+
+        results = cursor.fetchall()
+
+        # Create a list of dictionaries to hold results
+        food_items = []
+        for row in results:
+            food_items.append({
+                'name': row[0],
+                'calories': row[1],
+                'protein': row[2],
+                'carbs': row[3],
+                'fat': row[4]
+            })
+
+        cursor.close()
+
+        return render_template('search.html', food_items=food_items)
+
+    # Handle the case for GET requests or initial page load
+    return render_template('search.html')
+
+
+@app.route('/select_food', methods=['POST'])
+def select_nutrition():
+    #Can input cals fat and carbs as parameter or add the search into this function and save as variables
+    try:
+        # Retrieve user input from the form
+        #unsure if this interacts correctly with ui
+        selected_source = request.form['selected_source']
+        selected_restaurant = request.form['selected_restaurant']
+        order_by_clause = ""
+        for attribute in selected_attributes:
+            if attribute == "calories":
+                order_by_clause +=f"ABS(gf.{attribute/10} - %s) + "
+            order_by_clause += f"ABS(gf.{attribute} - %s) + "
+
+        order_by_clause = order_by_clause.rstrip("+ ")
+        # Validate input
+        if not selected_food or not selected_attributes or not selected_source:
+            return jsonify({"error": "Invalid input. Please provide all required fields."})
+
+        # Define the columns to be selected based on user input
+        selected_columns = ", ".join(selected_attributes)
+
+        # Query the appropriate table based on the selected source
+        cursor = mysql.connection.cursor()
+
+        if selected_source == "Grubhub":
+            cursor.execute(f"""SELECT gf.name, calories, total_fat, total_carbs, protein FROM grubhub_food gf
+                               JOIN grubhub_available ga ON gf.food_id = ga.food_id
+                               JOIN grubhub_restaurant gr ON ga.restaurant_id = gr.restaurant_id
+                               WHERE gr.name = %s
+                               ORDER BY {order_by_clause} 
+                               LIMIT 1;""", (selected_restaurant, cals/10, fat, carbs, protein)) #cals, fat, carbs, protein is those stats for the originally selected food
+        elif selected_source == "Dining Hall":
+            cursor.execute(f"""SELECT ndhfood.name, calories, total_fat, total_carbohydrate, protein FROM ndhfood
+                               ORDER BY {order_by_clause}
+                               LIMIT 1;""", (cals/10, fat, carbs, protein))
+        else:
+            return jsonify({"error": "Invalid source selected."})
+
+        selected_item = cursor.fetchone()
+        cursor.close()
+
+        if not selected_item:
+            return jsonify({"error": "No matching item found."})
+
+        # Format the result as a JSON response
+        result = {}
+        for i, attribute in enumerate(selected_attributes):
+            result[attribute] = selected_item[i]
+
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 if __name__ == '__main__':
