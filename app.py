a3506ee75614ab058698cadf4e1f7c09daa00545
Yanni Vu <yvu@nd.edu>
2023-12-04 21:01:16 -0500

    WEBSITE DONE!

--- app.py
+++ app.py
@@ -179,8 +179,7 @@ def delete_user():
             mysql.connection.commit()
             cursor.close()
     
-    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
-    return f"{user_to_delete} successfully deleted<br>{back_button}"
+    return render_template('user_delete.html', user=user_to_delete)
 
 
 @app.route('/add_user', methods=['POST'])
@@ -196,16 +195,16 @@ def add_user():
     back_button = f'<a href="{url_for("admin_page")}">Back</a>'
 
     if existing_user:
-        # If the user already exists, return confirmation with a back button
-        confirmation_with_back = f"{confirmation_message}<br>{back_button}"
+        # If the user already exists, update the password
+        cur.execute("UPDATE users SET password = %s WHERE username = %s", (pw, user))
+        mysql.connection.commit()
+        cur.close()
     else:
         cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (user, pw))
         mysql.connection.commit()
         cur.close()
-        # In the 'else' branch, return confirmation with the back button as well
-        confirmation_with_back = f"{confirmation_message}<br>{back_button}"
 
-    return confirmation_with_back
+    return render_template('user_confirmation.html', user=user, pw=pw, existing_user=existing_user)
 
 
 @app.route('/confirm_update', methods=['GET'])
@@ -216,7 +215,9 @@ def confirm_update():
 
 @app.route('/add_item', methods=['POST'])
 def add_item():
-    query = None  # Initialize the query variable
+    error_message = None
+    existing_grubhub_item = False
+    existing_item = False
 
     try:
         # Retrieve the form data
@@ -229,58 +230,70 @@ def add_item():
         source = request.form['source']
 
         if source == "Dining Hall":
-            # Retrieve Dining Hall specific form data
             if 'meal' in request.form and 'day' in request.form:
                 meal_type = request.form['meal']
                 day = request.form['day']
                 cursor = mysql.connection.cursor()
-                query = "INSERT INTO ndhfood (name, calories, protein, total_fat, total_carbohydrate, category, meal_type, day) VALUES ('{}', {}, {}, {}, {}, '{}', '{}', '{}');".format(name, cals, protein, fat, carbs, category, meal_type, day)
-                cursor.execute(query)
+                
+                # Check if the item already exists in the database
+                cursor.execute("SELECT * FROM ndhfood WHERE name = %s", (name,))
+                existing_item = cursor.fetchone()
+
+                if existing_item:
+                    # Update the existing item
+                    update_query = "UPDATE ndhfood SET calories = %s, protein = %s, total_fat = %s, total_carbohydrate = %s, category = %s, meal_type = %s, day = %s WHERE name = %s"
+                    cursor.execute(update_query, (cals, protein, fat, carbs, category, meal_type, day, name))
+                else:
+                    # Insert a new item
+                    insert_query = "INSERT INTO ndhfood (name, calories, protein, total_fat, total_carbohydrate, category, meal_type, day) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                    cursor.execute(insert_query, (name, cals, protein, fat, carbs, category, meal_type, day))
+
                 mysql.connection.commit()
                 cursor.close()
-                print(f'query executed: {query}')
-                success_message = f"Record updated successfully. Query: {query}"
-            else:
-                raise KeyError(f'Missing Dining Hall specific fields. Query: {query}')
         elif source == "Grubhub":
             restaurant_name = request.form['restaurant']
             price = float(request.form['price']) 
             cursor = mysql.connection.cursor()
-            cursor.execute("SELECT restaurant_id FROM grubhub_restaurant WHERE name = %s", (restaurant_name,))
-            restaurant_id = cursor.fetchone()[0]
-            cursor.close()
+            
+            # Check if the item already exists in the database for a given restaurant
+            cursor.execute("SELECT * FROM grubhub_food AS f JOIN grubhub_available AS a ON f.id = a.food_id JOIN grubhub_restaurant AS r ON a.restaurant_id = r.restaurant_id WHERE f.name = %s AND r.name = %s", (name, restaurant_name,))
+            existing_grubhub_item = cursor.fetchone()
 
-            cursor = mysql.connection.cursor()
-            cursor.execute("INSERT INTO grubhub_food (name, calories, protein, total_fat, total_carbs, type, price) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-                           (name, cals, protein, fat, carbs, category, price))
-            food_id = cursor.lastrowid  # Get the last inserted food_id
-            cursor.close()
+            if existing_grubhub_item:
+                # Update the existing item for the restaurant
+                update_query = "UPDATE grubhub_food SET calories = %s, protein = %s, total_fat = %s, total_carbs = %s, type = %s, price = %s WHERE name = %s"
+                cursor.execute(update_query, (cals, protein, fat, carbs, category, price, name))
+            else:
+                # Insert a new item for the restaurant
+                insert_query = "INSERT INTO grubhub_food (name, calories, protein, total_fat, total_carbs, type, price) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(insert_query, (name, cals, protein, fat, carbs, category, price))
+                food_id = cursor.lastrowid  # Get the last inserted food_id
+                
+                # Get restaurant_id for the given restaurant name
+                cursor.execute("SELECT restaurant_id FROM grubhub_restaurant WHERE name = %s", (restaurant_name,))
+                restaurant_id = cursor.fetchone()[0]
+                
+                # Associate the food with the restaurant
+                cursor.execute("INSERT INTO grubhub_available (restaurant_id, food_id) VALUES (%s, %s)", (restaurant_id, food_id))
 
-            cursor = mysql.connection.cursor()
-            cursor.execute("INSERT INTO grubhub_available (restaurant_id, food_id) VALUES (%s, %s)", (restaurant_id, food_id))
             mysql.connection.commit()
             cursor.close()
-            success_message = "Record updated successfully"
         
-    except Exception as e:
+    except (KeyError, ValueError, TypeError, IndexError) as e:
         error_message = f"An error occurred: {str(e)}"
 
-    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
-    if "error_message" in locals():
-        return f"{error_message}<br>{back_button}"
+    if existing_grubhub_item or existing_item:
+        update = True
     else:
-        return f"{success_message}<br>{back_button}"
+        update = False
+
+    return render_template('add_confirmation.html', source=source, name=name, cals=cals, protein=protein, fat=fat, carbs=carbs, error_message=error_message, update=update)
 
 
 
 @app.route('/search_user', methods=['POST'])
 def search_user():
-    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
-
     username = request.form['username']
-
-    if not username:
-        return "Please enter a username."
 
     # Perform a database query to retrieve information about the specified user
     cursor = mysql.connection.cursor()
@@ -292,26 +305,34 @@ def search_user():
         # Access the elements of the tuple by index
         username = user_info[0]
         password = user_info[1]
-
-        return f"User: {username} | Password: {password}<br>{back_button}"
+        return render_template('user_search.html', username=username, password=password, exist=True)
     else:
-        return f"User not found.<br>{back_button}"
+        return render_template('user_search.html', exist=False)
+
 
 @app.route('/delete_item', methods=['POST'])
 def delete_item():
+    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
     if request.method == 'POST':
         item_to_delete = request.form['item_to_delete']
         option = request.form['option']
         if item_to_delete:
             cursor = mysql.connection.cursor()
             if option == "Grubhub":
+                restaurant = request.form['restaurant']
                 cursor = mysql.connection.cursor()
-                cursor.execute("DELETE FROM grubhub_food WHERE name = %s", (item_to_delete,))
+                cursor.execute("DELETE FROM grubhub_food WHERE name = %s and restaurant = %s", (item_to_delete, restaurant))
+                mysql.connection.commit()
+                cursor.close()
+            elif option == "Dining Hall":
+                meal = request.form['meal_delete']
+                day = request.form['day_delete']
+                cursor = mysql.connection.cursor()
+                cursor.execute("DELETE FROM ndhfood WHERE name = %s and meal_type = %s and day = %s", (item_to_delete, meal, day))
                 mysql.connection.commit()
                 cursor.close()
     
-    back_button = f'<a href="{url_for("admin_page")}">Back</a>'
-    return f"{item_to_delete} successfully deleted<br>{back_button}"
+    return render_template('delete_confirmation.html', item_to_delete=item_to_delete, option=option)
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
@@ -402,18 +423,20 @@ def display_similar():
     
     if chosen_sort == "nutrition":
         order_by_clause = ""
-        for value in nutritional_values:
-            if value == 'calories':
-                order_by_clause += f"ABS(gf.calories/10 - {calories}/10) + "
-            elif value == 'protein':
-                order_by_clause += f"ABS(gf.protein - {protein}) + "
-            elif value == 'carbs':
-                order_by_clause += f"ABS(gf.total_carbs - {carbs}) + "
-            elif value == 'fat':
-                order_by_clause += f"ABS(gf.total_fat - {fat}) + "
+        if nutritional_values:
+            for value in nutritional_values:
+                if value == 'calories':
+                    order_by_clause += f"ABS(gf.calories/10 - {calories}/10) + "
+                elif value == 'protein':
+                    order_by_clause += f"ABS(gf.protein - {protein}) + "
+                elif value == 'carbs':
+                    order_by_clause += f"ABS(gf.total_carbs - {carbs}) + "
+                elif value == 'fat':
+                    order_by_clause += f"ABS(gf.total_fat - {fat}) + "
+            order_by_clause = order_by_clause[:-3]
+        else:
+            order_by_clause = f"ABS(gf.calories/10 - {calories}/10) + ABS(gf.protein - {protein}) + ABS(gf.total_carbs - {carbs}) + ABS(gf.total_fat - {fat})"
 
-        # Remove the last '+' and extra spaces
-        order_by_clause = order_by_clause[:-3]
 
         query_all = f"""SELECT gf.name, calories, protein, total_carbs, total_fat FROM grubhub_food gf
                 JOIN grubhub_available ga ON gf.food_id = ga.food_id
